1bc0520bb0eb4b7eb47885c76c5f36c05e0f9e53
devstefancho <skku7714@gmail.com>
2021-10-05 23:52:23 +0900

    bugfix: app.use 로직 수정
    - cell과 관련된 route가 먼저 use 되고 나서, proxy를 시도해야함

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -11,6 +11,8 @@ export const serve = (
 ) => {
   const app = express();
 
+  app.use(createCellsRouter(filename, dir));
+
   if (useProxy) {
     // if NODE_ENV is not production, use another localhost(CRA server)
     app.use(
@@ -24,8 +26,6 @@ export const serve = (
     const packagePath = require.resolve('local-client/build/index.html');
     app.use(express.static(path.dirname(packagePath)));
   }
-
-  app.use(createCellsRouter(filename, dir));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
